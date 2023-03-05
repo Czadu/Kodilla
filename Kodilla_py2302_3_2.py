3704491d626b7ed7835c7753af27af764e4b8304
Cezary Grzesiuk <cezary.grzesiuk@gmail.com>
2023-03-05 17:26:36 +0100

    Add shop Kodilla_py2302_3_2.py

--- Kodilla_py2302_3_2.py
+++ Kodilla_py2302_3_2.py
@@ -1,5 +1,16 @@
 
 shopping_list = {
             "piekarnia" : ["Chleb", "Paczek", "Bulki"],
-            "warzywniak" : ['Marchew', 'Seler', 'Rukola']
+            "warzywniak" : ['Marchew', 'Seler', 'Rukola'],
+            "biedronka" : ['Ser', 'Maslo', 'Makaron']
 }
+
+x = 0
+print()
+for shop, items in shopping_list.items():
+    print(f"idę do {shop.capitalize()} i kupuję {items}" "\n")
+    for items in shopping_list[shop]:
+        x += 1
+        
+print (f"w sumie kupuję {x} produktów")
+print()
